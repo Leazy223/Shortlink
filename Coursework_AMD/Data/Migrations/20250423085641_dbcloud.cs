d71b04818c13bca013194e6b286499073c4bee17
Leazy <135291669+Leazy223@users.noreply.github.com>
2025-04-26 20:55:08 +0700

    Fix code

---
+++ Coursework_AMD/Data/Migrations/20250423085641_dbcloud.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Coursework_AMD.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class dbcloud : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
